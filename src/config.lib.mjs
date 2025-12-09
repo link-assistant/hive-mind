da77d0b04d51eb8cf13ae5254a2fe8c9cd4c89cc
konard <drakonard@gmail.com>
2025-12-09 06:06:32 +0100

    Remove opencode timeout configuration from config.lib.mjs

--- src/config.lib.mjs
+++ src/config.lib.mjs
@@ -40,7 +40,6 @@ const parseFloatWithDefault = (envVar, defaultValue) => {
 // Timeout configurations (in milliseconds)
 export const timeouts = {
   claudeCli: parseIntWithDefault('HIVE_MIND_CLAUDE_TIMEOUT_SECONDS', 60) * 1000,
-  opencodeCli: parseIntWithDefault('HIVE_MIND_OPENCODE_TIMEOUT_SECONDS', 60) * 1000,
   codexCli: parseIntWithDefault('HIVE_MIND_CODEX_TIMEOUT_SECONDS', 60) * 1000,
   githubApiDelay: parseIntWithDefault('HIVE_MIND_GITHUB_API_DELAY_MS', 5000),
   githubRepoDelay: parseIntWithDefault('HIVE_MIND_GITHUB_REPO_DELAY_MS', 2000),

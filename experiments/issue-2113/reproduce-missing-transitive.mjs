50698e2a564bddeef28d5628e2eb950e148afc8f
konard <drakonard@gmail.com>
2026-07-28 17:07:21 +0000

    test(2113): lint hermetic dependency experiment

--- experiments/issue-2113/reproduce-missing-transitive.mjs
+++ experiments/issue-2113/reproduce-missing-transitive.mjs
@@ -9,6 +9,7 @@ import assert from 'node:assert/strict';
 import { mkdtemp, mkdir, rm, writeFile } from 'node:fs/promises';
 import { tmpdir } from 'node:os';
 import { join } from 'node:path';
+import process from 'node:process';
 import { pathToFileURL } from 'node:url';
 import { useWithRetry } from '../../src/use-with-retry.lib.mjs';
 
